e69aa05d6c1ca8c1e858709cea01325244f72315
Adphi <philippe.adrien.nousse@gmail.com>
2024-10-07 13:59:18 +0200

    move clippy rules to workspace
    Signed-off-by: Adphi <philippe.adrien.nousse@gmail.com>

--- prost-reflect-validate/src/lib.rs
+++ prost-reflect-validate/src/lib.rs
@@ -1,15 +1,4 @@
 #![doc = include_str!("../README.md")]
-#![deny(warnings)]
-#![warn(unused_extern_crates)]
-#![deny(clippy::todo)]
-#![deny(clippy::unimplemented)]
-#![warn(clippy::unwrap_used)]
-#![deny(clippy::expect_used)]
-#![deny(clippy::panic)]
-#![deny(clippy::unreachable)]
-#![deny(clippy::await_holding_lock)]
-#![deny(clippy::needless_pass_by_value)]
-#![deny(clippy::trivially_copy_pass_by_ref)]
 
 use crate::registry::REGISTRY;
 use prost_reflect::ReflectMessage;
