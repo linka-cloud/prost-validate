2a2642e7ebe40bcc40a82ae33745423bb004acc8
Adphi <philippe.adrien.nousse@gmail.com>
2025-01-09 13:26:22 +0100

    add usage with tonic-build doc
    Signed-off-by: Adphi <philippe.adrien.nousse@gmail.com>

--- example-proto/build.rs
+++ example-proto/build.rs
@@ -1,8 +1,12 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let files = &["message.proto"];
+    let files = &["message.proto", "service.proto"];
     let includes = &["proto", "../prost-validate-types/proto"];
 
-    let mut config = prost_build::Config::new();
+    let mut config = {
+        let mut c = prost_build::Config::new();
+        c.service_generator(tonic_build::configure().service_generator());
+        c
+    };
 
     prost_validate_build::Builder::new().configure(&mut config, files, includes)?;
 
